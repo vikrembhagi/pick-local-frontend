05fc60fd979f54da8a713a3f6ad4a2b6fad821d0
Vikrem Bhagi <bhagi.50.singh@gmail.com>
2021-04-25 19:23:15 -0700

    Basic Setup

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Pick Local</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 class="text-6xl text-blue-200" >
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
